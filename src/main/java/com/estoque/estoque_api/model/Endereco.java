c85d92432f24bce4f208089c0a1f0117889c2cd0
adriano328 <lucasadrianodias@gmail.com>
2025-09-22 19:49:39 -0400

    Envio api.

---
+++ src/main/java/com/estoque/estoque_api/model/Endereco.java
@@ -0,0 +1,37 @@
+package com.estoque.estoque_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity @Table(name = "endereco")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "logradouro")
+    private String logradouro;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "complemento")
+    private String complemento;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "cep")
+    private String cep;
+
+    @Column(name = "uf")
+    private String uf;
+
+    @Column(name = "municipio")
+    private String municio;
+}
--- src/main/java/com/estoque/estoque_api/model/Fornecedor.java
+++ src/main/java/com/estoque/estoque_api/model/Fornecedor.java
@@ -1,5 +1,36 @@
 package com.estoque.estoque_api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
+
+@Entity @Table(name = "fornecedor")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "razaosocial")
+    private String razaoSocial;
+
+    @Column(name = "nomefantasia")
+    private String nomeFantasia;
+
+    @Column(name = "situacao")
+    private String situacao;
+
+    @Column(name = "datacadastral")
+    private Date dataCadastral;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefone")
+    private String telefone;
 }
